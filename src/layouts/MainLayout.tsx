3f63aec64bf4bf23c27a8b598c7fa7fe647ff7ed
muzi <xiumuds@163.com>
2024-07-28 22:26:19 +0800

    更新首页和404

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,28 @@
 import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom'
+import { Layout } from 'antd'
+import styles from './MainLayout.module.scss'
+import Logo from '../components/Logo'
+import UserInfo from '../components/UserInfo'
+
+const { Header, Content, Footer } = Layout
 
 const MainLayout: FC = () => {
   return (
-    <>
-      <div>MainLayout header</div>
-      <div>
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>
+          <Logo />
+        </div>
+        <div className={styles.right}>
+          <UserInfo />
+        </div>
+      </Header>
+      <Content className={styles.main}>
         <Outlet />
-      </div>
-      <div>MainLayout footer</div>
-    </>
+      </Content>
+      <Footer className={styles.footer}>小木问卷 &copy;2024 - preset. Created by Muzi</Footer>
+    </Layout>
   )
 }
 
